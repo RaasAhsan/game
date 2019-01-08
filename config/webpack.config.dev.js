85bd8c49b5043a5a1d601dd1e737f3e5551b4a6c
Raas Ahsan <agro@jantox.com>
2019-01-08 01:27:50 -0600

    Initial commit with mdn example

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9090,
+    historyApiFallback: true
+  },
+  entry: [
+    './src/main.js'
+  ],
+  output: {
+    pathinfo: true,
+    filename: 'static/js/bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html'
+    })
+  ]
+};
