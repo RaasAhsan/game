dc51488c2b48a91c9e680fc09bd541f8b884389e
Raas Ahsan <agro@jantox.com>
2019-01-08 19:18:08 -0600

    rotate square

--- src/main.js
+++ src/main.js
@@ -24,7 +24,20 @@ const programInfo = {
 };
 
 const buffers = initBuffers(gl);
-drawScene(gl, programInfo, buffers);
+
+let then = 0;
+
+function render(now) {
+  now *= 0.001;
+  const delta = now - then;
+  then = now;
+
+  drawScene(gl, programInfo, buffers, delta);
+
+  requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
 
 function initializeShaders(gl, vsSource, fsSource) {
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
@@ -62,6 +75,7 @@ function initBuffers(gl) {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   const positions = [
+    0.0, 0.0,
     -1.0,  1.0,
      1.0,  1.0,
     -1.0, -1.0,
@@ -75,7 +89,9 @@ function initBuffers(gl) {
   };
 }
 
-function drawScene(gl, programInfo, buffers) {
+let rotation = 0.0;
+
+function drawScene(gl, programInfo, buffers, delta) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0);
   gl.enable(gl.DEPTH_TEST);
@@ -92,15 +108,21 @@ function drawScene(gl, programInfo, buffers) {
   mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
 
   const modelViewMatrix = mat4.create();
-  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+
+  rotation += delta * 30;
+  
+  mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, 0.0, -6.0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, rotation * Math.PI / 180, [0, 1, 0]);
 
   {
     const numComponents = 2;  // pull out 2 values per iteration
     const type = gl.FLOAT;    // the data in the buffer is 32bit floats
     const normalize = false;  // don't normalize
-    const stride = 0;         // how many bytes to get from one set of values to the next
+    // How much data is stored in the buffer per vertex
+    const stride = 8;         // how many bytes to get from one set of values to the next
                               // 0 = use type and numComponents above
-    const offset = 0;         // how many bytes inside the buffer to start from
+    // How many bytes from the beginning does the data start
+    const offset = 8;
 
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, numComponents, type, normalize, stride, offset);
