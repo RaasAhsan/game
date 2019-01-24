e623ea008c60f6302edc10f52ee60bb54f676d8e
Raas Ahsan <agro@jantox.com>
2019-01-23 23:24:50 -0600

    organize by section

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,13 +4,17 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div>
-      <h2>Demos</h2>
-      <div>
-        <Link to="/metaballs">Metaballs</Link>
-      </div>
-      <div>
-        <Link to="/shader">Shader</Link>
-      </div>
+      <h1>Demos</h1>
+      
+      <section>
+        <h2>Graphics</h2>
+        <div>
+          <Link to="/metaballs">Metaballs</Link>
+        </div>
+        <div>
+          <Link to="/shader">Shader</Link>
+        </div>
+      </section>
     </div>
   );
 }
